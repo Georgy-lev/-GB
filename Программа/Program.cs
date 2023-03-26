b412dc2f0213501e918d00aa779296d7245d4afb
Georgy <x.ad@yandex.ru>
2023-03-26 21:37:53 +0300

    Создан репозиторий на gh

---
+++ Программа/Program.cs
@@ -0,0 +1,9 @@
+﻿//Напишите программу, которая выдает максимальное число массива.
+
+int[] numbers = new int[3]{2,3,7}; 
+int max = numbers[0]; 
+for (int index = 0; index < numbers.Length; index++) 
+{ 
+ if (numbers[index] > max) max = numbers[index]; 
+} 
+Console.WriteLine(max);
